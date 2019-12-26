4f47f6c83af0aa094d40b10b56b8290f407136fd
lgramirez <gramirez.developer@gmail.com>
2019-12-26 17:45:50 -0400

    Add layout and send it to the view

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Home';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Layout = () => (
+    <div className="Layout">
+        <Header />
+        <Home />
+        <Footer />
+    </div>
+);
+
+export default Layout;
