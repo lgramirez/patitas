f7e4b655b62b344b02f3998119455d81d73ec76e
lgramirez <gramirez.developer@gmail.com>
2019-12-30 19:07:30 -0400

    Add .env webpack plugin

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCI-oJocmaIpcvEPQnykqkGnErpWUu_9mE",
-    authDomain: "patitas-9da3d.firebaseapp.com",
-    databaseURL: "https://patitas-9da3d.firebaseio.com",
-    projectId: "patitas-9da3d",
-    storageBucket: "patitas-9da3d.appspot.com",
-    messagingSenderId: "680959822544",
-    appId: "1:680959822544:web:2994e4840bd767a022ecd5",
-    measurementId: "G-0HZV36LXXJ"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
