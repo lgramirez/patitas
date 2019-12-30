d9c652c98cf169f253c24f9936d71d5acf67a4d2
lgramirez <gramirez.developer@gmail.com>
2019-12-30 17:29:41 -0400

    Add react roter, component for 404

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import Home from '../pages/Home';
 import Header from './Header';
 import Footer from './Footer';
-import Form from './Form';
-import Login from '../pages/Login';
-import Dashboard from '../pages/Dashboard';
 
 const Layout = props => (
     <div className="Layout">
