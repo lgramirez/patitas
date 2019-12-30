44bfc476ac4e059229d06e3cc55b498f58c6b032
lgramirez <gramirez.developer@gmail.com>
2019-12-30 19:54:00 -0400

    Try the facebook login and logout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import { auth, provider } from '../utils/firebase';
 
-const Login = () => (
+const Login = () => {
+
+    const loginFacebook = () => {
+        auth().signInWithPopup(provider)
+            .then(({ user }) => {
+                console.log(user);
+            });
+    }
+
+    const logoutFacebook = () => {
+        auth().signOut()
+            .then(() => {
+                console.log('cerrar sesion');
+            });
+    }
+
+    return(
     <div className="Login">
         <div className="Login-container">
             <div className="Login-content">
                 <h2>Crear cuenta o Iniciar sesión</h2>
-                <button>
-                    <i>FB logo</i>
+                <button onClick={loginFacebook}>
+                    <i className="fab fa-facebook-square" />
                     <span>Iniciar sesión con Facebook</span>
+                </button>
+                <button onClick={logoutFacebook}>
+                    Cerrar Sesión
                 </button>
             </div>
         </div>
     </div>
-);
+    );
+}
 
 export default Login;
