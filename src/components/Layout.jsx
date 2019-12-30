4a99d5c3ab504a15f3ad662312a89a0d107f72f5
lgramirez <gramirez.developer@gmail.com>
2019-12-30 17:32:38 -0400

    Prepare to receive component from App

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Home from '../pages/Home';
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = props => (
     <div className="Layout">
         <Header />
-        <Home />
+        {props.children}
         <Footer />
     </div>
 );
