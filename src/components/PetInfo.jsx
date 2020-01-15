64f4831f0ec1c6f6bde0c5c0c2f98b62b5d1b20e
lgramirez <gramirez.developer@gmail.com>
2020-01-15 12:09:02 -0400

    New component to display details of a specific pet

---
+++ src/components/PetInfo.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const PetInfo = props => {
+    return (
+        <div className="PetInfo">
+            <div className="PetInfo-container">
+                <div className="PetInfo-content">
+                    <div className="PetInfo-avatar">
+                        <img src={props.pet.photo} alt={props.pet.name}/>
+                    </div>
+                    <div className="PetInfo-profile">
+                        <h2>{props.pet.name}</h2>
+                        <span>
+                            {props.pet.type === 'dog' && '🐶'}
+                            {props.pet.type === 'cat' && '🐱'}
+                        </span>
+                        <p>{props.pet.description}
+                            <i>
+                                {props.pet.gender === 'male' && '♂'}
+                                {props.pet.gender === 'female' && '♀'}
+                            </i>
+                        </p>
+                        <div className="PetInfo-tyoe">
+                            <button>Adoptar</button>
+                        </div>
+                        <div className="PetInfo-profile-adopt">
+                            <div className="PetInfo-item">
+                                <h3>Datos de Contacto:</h3>
+                                <span>Dueño:</span>
+                                <h4>Gonzalo Ramirez</h4>
+                                <span>Correo:</span>
+                                <h4>gramirez.developer@gmail.com</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PetInfo;
